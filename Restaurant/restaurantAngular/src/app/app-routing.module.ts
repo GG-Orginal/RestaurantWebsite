6e7e1f20d03d62781a22df9591d94ce3cec7cd60
GGILL <ggill@cosmodigital.com>
2021-12-16 22:01:52 -0600

    search reservation

--- Restaurant/restaurantAngular/src/app/app-routing.module.ts
+++ Restaurant/restaurantAngular/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import {CartComponent} from "./cart/cart.component";
 import {CheckoutComponent} from "./checkout/checkout.component";
 import {OrderconfirmedComponent} from "./orderconfirmed/orderconfirmed.component";
 import {HomepageComponent} from "./homepage/homepage.component";
+import {SearchReservationComponent} from "./search-reservation/search-reservation.component";
 
 const routes: Routes = [
   {path: 'menu', component: MenuComponent},
   {path: 'review', component: ReviewComponent},
   {path: 'cart', component: CartComponent},
   {path: 'reservation', component: MakeReservationComponent},
+  {path: 'searchreservation', component: SearchReservationComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'orderconfirmed', component: OrderconfirmedComponent},
   {path: '**', component: HomepageComponent},
--- Restaurant/restaurantAngular/src/app/search-reservation/search-reservation.component.ts
+++ Restaurant/restaurantAngular/src/app/search-reservation/search-reservation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ReservationService} from "../service/reservation.service";
+import {Router} from "@angular/router";
+import {Reservation} from "../model/Reservation";
 
 @Component({
   selector: 'app-search-reservation',
@@ -7,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchReservationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public reservationService:ReservationService,private router:Router) { }
+
+  searchtitle: string = "By Phone";
+  placeholdertext: string = "Phone Number";
+  input: string = "";
+  reservations: Reservation[] = [];
+  noresults: string = "";
 
   ngOnInit(): void {
 
   }
 
+  searchPhone(){
+    this.searchtitle = "By Phone";
+    this.placeholdertext = "Phone Number";
+  }
+
+  searchConfNo(){
+    this.searchtitle = "By Confirmation Number";
+    this.placeholdertext = "Confirmation Number";
+  }
+
+  search(){
+
+    var searchno = "";
+
+    if (this.searchtitle == "By Phone") {
+      searchno = 'p' + this.input;
+    }
+    if (this.searchtitle == "By Confirmation Number"){
+      searchno = 'c' + this.input;
+    }
+
+    this.reservationService.searchReservation(searchno).subscribe(resp => {
+
+      this.reservations = [];
+
+      if (resp.length == 0){
+        this.noresults = "No Results :(";
+        return;
+      }
+
+      this.noresults = "";
+
+      for(let i = 0; i < resp.length; i++){
+        let resv = new Reservation();
+
+        resv.name = resp[i].name;
+        resv.phoneNumber = resp[i].phoneNumber;
+        resv.location = resp[i].location;
+        resv.datetime = resp[i].datetime;
+        resv.howMany = resp[i].howMany;
+        resv.confirmationNumber = resp[i].confirmationNumber;
+
+        this.reservations.push(resv);
+      }
+
+    });
+
+  }
 }
